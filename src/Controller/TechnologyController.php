184670b8ea17454a992e7ad8a61ea74c159622da
Cl-laus <lucasluisetti@gmail.com>
2025-12-11 14:05:31 +0100

    finalisationTechnoRepo

--- src/Controller/TechnologyController.php
+++ src/Controller/TechnologyController.php
@@ -1,13 +1,17 @@
 <?php
+
 namespace App\Controller;
 
 use App\Entity\Technology;
 use App\Repository\TechnologyRepository;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api/technologies', name: 'api_technologies_')]
@@ -16,7 +20,6 @@ final class TechnologyController extends AbstractController
     public function __construct(
         private TechnologyRepository $technologyRepository,
         private SerializerInterface $serializer
-
     ) {
     }
 
@@ -24,19 +27,32 @@ final class TechnologyController extends AbstractController
     public function showList(): JsonResponse
     {
         $technologies = $this->technologyRepository->findAll();
-        $data         = $this->serializer->serialize($technologies, 'json', context: ['groups' => 'technology:read']);
+        $data = $this->serializer->serialize(
+            $technologies,
+            'json',
+            context: ['groups' => 'technology:read']
+        );
+
         return new JsonResponse($data, JsonResponse::HTTP_OK, [], true);
     }
 
     #[Route('/{id}', name: '_detail', methods: ['GET'], requirements: ['id' => Requirement::DIGITS])]
-    public function showDetail(Technology $technology): JsonResponse
-    {
-        $data = $this->serializer->serialize($technology, 'json', context: ['groups' => 'technology:read']);
+    public function showDetail(
+        #[MapEntity] Technology $technology
+    ): JsonResponse {
+        $data = $this->serializer->serialize(
+            $technology,
+            'json',
+            context: ['groups' => 'technology:read']
+        );
+
         return new JsonResponse($data, JsonResponse::HTTP_OK, [], true);
     }
 
     #[Route('', name: '_create', methods: ['POST'])]
-    public function create(#[MapRequestPayload(serializationContext: ['groups' => 'technology:write'])]Technology $newTechnology
+    public function create(
+        #[MapRequestPayload(serializationContext: ['groups' => 'technology:write'])]
+        Technology $newTechnology
     ): JsonResponse {
         $this->technologyRepository->save($newTechnology);
 
@@ -49,9 +65,30 @@ final class TechnologyController extends AbstractController
         return new JsonResponse($data, JsonResponse::HTTP_CREATED, [], true);
     }
 
+    #[Route('/{id}', name: '_update', methods: ['PATCH'])]
+    public function update(
+        #[MapEntity] Technology $technology,
+        Request $request
+    ): JsonResponse {
+        $this->serializer->deserialize(
+            $request->getContent(),
+            Technology::class,
+            'json',
+            [
+                AbstractNormalizer::OBJECT_TO_POPULATE => $technology,
+                'groups' => ['technology:write'],
+            ]
+        );
+
+        $this->technologyRepository->save($technology);
+
+        return $this->showDetail($technology);
+    }
+
     #[Route('/{id}', name: '_delete', methods: ['DELETE'], requirements: ['id' => Requirement::DIGITS])]
-    public function delete(Technology $technology): JsonResponse
-    {
+    public function delete(
+        #[MapEntity] Technology $technology
+    ): JsonResponse {
         $this->technologyRepository->delete($technology);
 
         return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
