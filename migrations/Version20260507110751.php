363b6cf32fdcf4b917b3ccb136de3dae226aa226
Lucas Luisetti <lucasluisetti@gmail.com>
2026-05-07 13:10:16 +0200

    feat: add migration for Information careerTitle and careerText columns

---
+++ migrations/Version20260507110751.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260507110751 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE information ADD career_title VARCHAR(255) DEFAULT NULL, ADD career_text LONGTEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE information DROP career_title, DROP career_text');
+    }
+}
